03aef1162d43a21c6d1f997010aa2c89a0b918ce
Petr Sumbera <petr.sumbera@oracle.com>
2018-08-29 03:08:36 -0700

    28554763 Upgrade PHP to version 7.1.21 28554771 Upgrade PHP to version 5.6.37 28550716 problem in UTILITY/PHP 25120108 uw-imap missing from php

--- components/php/php56/enable-extensions.mk
+++ components/php/php56/enable-extensions.mk
@@ -164,11 +164,11 @@ CONFIGURE_OPTIONS += --enable-zip=shared
 
 # Special cases
 
-# IMAP: commented out until IMAP library itself is approved
-#CONFIGURE_OPTIONS += --with-imap=shared,$(PHP_TOP_DIR)/uw-imap/build/$(MACH64)
-#CONFIGURE_OPTIONS += --with-imap-ssl=shared,/usr
-#CONFIGURE_OPTIONS += --with-kerberos	# WARNING! openssl reads same flag!
-#COMPONENT_PRE_CONFIGURE_ACTION += (cd $(PHP_TOP_DIR)/uw-imap ; gmake build) ;
+# IMAP
+CONFIGURE_OPTIONS += --with-imap=shared,$(PHP_TOP_DIR)/uw-imap/build/$(MACH64)
+CONFIGURE_OPTIONS += --with-imap-ssl=shared,/usr
+CONFIGURE_OPTIONS += --with-kerberos   # WARNING! openssl reads same flag!
+COMPONENT_PRE_CONFIGURE_ACTION += (cd $(PHP_TOP_DIR)/uw-imap ; gmake build) ;
 
 # Enables emulation of the obsolete mhash extension by the hash extension.
 CONFIGURE_OPTIONS += --with-mhash
