8998cfb18916b07c136e1e17a737069128e1bd57
Alexandr Nedvedicky <Alexandr.Nedvedicky@oracle.COM>
2018-09-06 03:18:39 -0700

    27558039 proftpd:ftp_transfer_done:transfer-done: invalid address received for FTP probe

--- components/proftpd/dtrace/ftp.d
+++ components/proftpd/dtrace/ftp.d
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009, 2015, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2009, 2018, Oracle and/or its affiliates. All rights reserved.
  */
 
 #pragma D depends_on library net.d
@@ -16,12 +16,11 @@ typedef struct ftpinfo {
  * This structure must match the definition of same in ftp_provider_impl.h.
  */
 typedef struct ftpproto {
-	uint32_t ftp_user;              /* user name */
-	uint32_t ftp_cmd;               /* FTP command */
-	uint32_t ftp_pathname;          /* path of file being operated upon */
-	uint32_t ftp_raddr;             /* remote address, as IPv6 address */
-	uint32_t ftp_fd;                /* fd for transfer, if any */
-	uint32_t ftp_pad;               /* padding for copyin() */
+	uintptr_t ftp_user;              /* user name */
+	uintptr_t ftp_cmd;               /* FTP command */
+	uintptr_t ftp_pathname;          /* path of file being operated upon */
+	uintptr_t ftp_raddr;             /* remote address, as IPv6 address */
+	uintptr_t ftp_fd;                /* fd for transfer, if any */
 	uint64_t ftp_nbytes;            /* bytes transferred, if any */
 } ftpproto_t;
 
@@ -29,20 +28,20 @@ typedef struct ftpproto {
 translator conninfo_t <ftpproto_t *f> {
 	ci_protocol = "tcp";
 	ci_remote = copyinstr((uintptr_t)
-	*(uint32_t *)copyin((uintptr_t)&f->ftp_raddr, sizeof (uint32_t)));
+	(uintptr_t)copyin((uintptr_t)&f->ftp_raddr, sizeof (uintptr_t)));
 	ci_local = "<unknown>";
 };
 
 #pragma D binding "1.6.1" translator
 translator ftpinfo_t <ftpproto_t *f> {
 	fti_user = copyinstr((uintptr_t)
-	    *(uint32_t *)copyin((uintptr_t)&f->ftp_user, sizeof (uint32_t)));
+	    *(uintptr_t *)copyin((uintptr_t)&f->ftp_user, sizeof (uintptr_t)));
 	fti_cmd = copyinstr((uintptr_t)
-	    *(uint32_t *)copyin((uintptr_t)&f->ftp_cmd, sizeof (uint32_t)));
+	    *(uintptr_t *)copyin((uintptr_t)&f->ftp_cmd, sizeof (uintptr_t)));
 	fti_pathname = copyinstr((uintptr_t)
-	    *(uint32_t *)copyin((uintptr_t)&f->ftp_pathname,
-	    sizeof (uint32_t)));
+	    *(uintptr_t *)copyin((uintptr_t)&f->ftp_pathname,
+	    sizeof (uintptr_t)));
 	fti_nbytes =
 	    *(uint64_t *)copyin((uintptr_t)&f->ftp_nbytes, sizeof (uint64_t));
-	fti_fd = *(uint32_t *)copyin((uintptr_t)&f->ftp_fd, sizeof (uint32_t));
+	fti_fd = (uintptr_t)copyin((uintptr_t)&f->ftp_fd, sizeof (uintptr_t));
 };
--- components/proftpd/dtrace/ftp_provider_impl.h
+++ components/proftpd/dtrace/ftp_provider_impl.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009, 2015, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2009, 2018, Oracle and/or its affiliates. All rights reserved.
  */
 
 #ifndef _FTP_PROVIDER_IMPL_H
@@ -9,24 +9,23 @@
  * This structure must match the definition of same in ftp.d.
  */
 typedef struct ftpproto {
-	uint32_t ftp_user;	/* user name */
-	uint32_t ftp_cmd;	/* FTP command */
-	uint32_t ftp_pathname;  /* path of file being operated upon */
-	uint32_t ftp_raddr;	/* remote address, as IPv6 address */
-	uint32_t ftp_fd;	/* fd for transfer, if any */
-	uint32_t ftp_pad;	/* padding for copyin() */
+	uintptr_t ftp_user;	/* user name */
+	uintptr_t ftp_cmd;	/* FTP command */
+	uintptr_t ftp_pathname;  /* path of file being operated upon */
+	uintptr_t ftp_raddr;	/* remote address, as IPv6 address */
+	uintptr_t ftp_fd;	/* fd for transfer, if any */
 	uint64_t ftp_nbytes;	/* bytes transferred, if any */
 } ftpproto_t;
 
 #define	FTP_TRANSFER_PROTO(proto, fh, len) \
 do { \
 	bzero((proto), sizeof (struct ftpproto)); \
-	(proto)->ftp_user = (uint32_t)session.user; \
-	(proto)->ftp_cmd = (uint32_t)session.curr_cmd; \
-	(proto)->ftp_pathname = (uint32_t)((fh)->fh_path); \
+	(proto)->ftp_user = (uintptr_t)session.user; \
+	(proto)->ftp_cmd = (uintptr_t)session.curr_cmd; \
+	(proto)->ftp_pathname = (uintptr_t)((fh)->fh_path); \
 	(proto)->ftp_raddr = \
-	    (int32_t)pr_netaddr_get_ipstr(session.c->remote_addr); \
-	(proto)->ftp_fd = (uint32_t)((fh)->fh_fd); \
+	    (uintptr_t)pr_netaddr_get_ipstr(session.c->remote_addr); \
+	(proto)->ftp_fd = (uintptr_t)((fh)->fh_fd); \
 	(proto)->ftp_nbytes = (len); \
 } while (0)
 
