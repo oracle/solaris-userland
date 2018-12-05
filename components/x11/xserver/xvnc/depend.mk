6dd9845766819bf44afa47f7cbd3984ed68cdca3
Niveditha Rau <Niveditha.Rau@Oracle.COM>
2018-12-04 19:54:06 -0800

    28978241 Update tigervnc to 1.9.0 27514592 Xvnc hangs loops forever calling select from rfb::SSecurityTLS::shutdown

--- components/x11/xserver/xvnc/depend.mk
+++
@@ -1,2 +0,0 @@
-# Delete this & Makefile section when build servers are installed with fltk
-x11/xserver/xvnc: x11/lib/fltk
--- components/x11/xserver/xvnc/files/vnclock.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/sbin/sh
-#
-# Copyright (c) 2016, Oracle and/or its affiliates. All rights reserved.
-#
-
-DISPLAY=":$1"
-XAUTHORITY="/var/xauth/$1"
-export XAUTHORITY DISPLAY
-/usr/bin/xdg-screensaver lock
