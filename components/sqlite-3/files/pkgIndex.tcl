277e7364ed784deea8ac784fc9c46940a4aa01ff
Libor Bukata <Libor.Bukata@oracle.com>
2019-04-12 01:26:09 -0700

    29127556 Update sqlite to version 3.28.0 29664348 problem in DATABASE/SQLITE3

--- components/sqlite-3/files/pkgIndex.tcl
+++ components/sqlite-3/files/pkgIndex.tcl
@@ -1,1 +1,1 @@
-package ifneeded sqlite3 3.17.0 [list load [file join $dir [expr $::tcl_platform(wordSize) * 8] libsqlite3.17.0.so] sqlite3]
+package ifneeded sqlite3 %%%SQLITE_VERSION%%% [list load [file join $dir [expr $::tcl_platform(wordSize) * 8] libsqlite%%%SQLITE_VERSION%%%.so] sqlite3]
