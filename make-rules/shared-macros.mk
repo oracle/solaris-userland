2de84f6fb32aacd5fa89c9d6f64d1093752baed0
Petr Sumbera <petr.sumbera@oracle.com>
2020-01-31 11:56:58 +0100

    30836245 Userland should build with GCC 9 again

--- make-rules/shared-macros.mk
+++ make-rules/shared-macros.mk
@@ -653,7 +653,7 @@ export PARFAIT_NATIVEGXX=$(GCC_ROOT)/bin/g++
 ONBLD_ROOT ?=	$(BUILD_TOOLS)/onbld
 ONBLD_BIN ?=	$(ONBLD_ROOT)/bin
 
-GCC_ROOT =	/usr/gcc/7
+GCC_ROOT =	/usr/gcc/9
 
 CC.studio.32 =	$(SPRO_VROOT)/bin/cc
 CXX.studio.32 =	$(SPRO_VROOT)/bin/CC
