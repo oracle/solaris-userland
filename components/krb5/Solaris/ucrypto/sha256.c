bd1190ab71abb049a91a1f17a3078674970945e6
Alexandr Nedvedicky <Alexandr.Nedvedicky@oracle.COM>
2021-04-01 22:44:44 +0200

    PSARC/2020/144 MIT Kerberos 1.18 32300993 upgrade kerberos to 1.18

--- components/krb5/Solaris/ucrypto/sha256.c
+++ components/krb5/Solaris/ucrypto/sha256.c
@@ -1,17 +1,25 @@
 /*
- * Copyright (c) 2017, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2021, Oracle and/or its affiliates.
  */
 
 #include "crypto_int.h"
 #include <libucrypto.h>
 
+/* ARGSUSED */
 krb5_error_code
-k5_sha256(const krb5_data *in, uint8_t out[K5_SHA256_HASHLEN])
+k5_sha256(const krb5_data *in, size_t blocks, uint8_t out[K5_SHA256_HASHLEN])
 {
     int r;
     size_t outlen = K5_SHA256_HASHLEN;
+    size_t i;
+    crypto_ctx_t	ctx;
 
-    r = ucrypto_digest(CRYPTO_SHA256, NULL, 0, (uchar_t *)in->data, 
-                       (size_t)in->length, (uchar_t *)out, &outlen);
+    r = ucrypto_digest_init(&ctx, CRYPTO_SHA256, NULL, 0);
+    for (i = 0; (i < blocks) && (r == CRYPTO_SUCCESS); i++)
+	    r = ucrypto_digest_update(&ctx, (uchar_t *)in[i].data, 
+	       in[i].length);
+    if (r == CRYPTO_SUCCESS)
+	    r = ucrypto_digest_final(&ctx, out, &outlen);
+
     return ((r == CRYPTO_SUCCESS) ? 0 : KRB5_CRYPTO_INTERNAL);
 }
