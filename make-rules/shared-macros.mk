2d9cc3935eaa10b1e4d9261e117e6197aa7d9783
Vladimir Marek <Vladimir.Marek@oracle.com>
2024-09-20 04:22:02 -0700

    PSARC/2024/081 EOF Perl 5.36 36666779 Remove perl 5.36 from UL

--- make-rules/shared-macros.mk
+++ make-rules/shared-macros.mk
@@ -805,9 +805,8 @@ PERL_VERSION_NODOT = $(subst .,,$(PERL_VERSION))
 # multiple packages for each version of perl listed here.  Used by
 # perl_modules/* but also used for those components that deliver a perl
 # package like graphviz and openscap.
-PERL_VERSIONS = 5.36 5.38
+PERL_VERSIONS = 5.38
 
-PERL.5.36 =     /usr/perl5/5.36/bin/perl
 PERL.5.38 =     /usr/perl5/5.38/bin/perl
 
 define test-perl-availability
