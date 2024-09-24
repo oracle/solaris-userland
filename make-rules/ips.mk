d730fabf74933c283a9cc5fbca7c578a8d237609
Jakub Kulik <jakub.kulik@oracle.com>
2024-09-24 01:51:42 -0700

    37069623 cleanup now unnecessary optional python dependencies

--- make-rules/ips.mk
+++ make-rules/ips.mk
@@ -84,7 +84,6 @@ PUBLISH_TRANSFORMS +=	$(WS_TOP)/transforms/depends
 PUBLISH_TRANSFORMS +=	$(WS_TOP)/transforms/devel
 PUBLISH_TRANSFORMS +=	$(WS_TOP)/transforms/docs
 PUBLISH_TRANSFORMS +=	$(LOCALE_TRANSFORMS)
-PUBLISH_TRANSFORMS +=	$(WS_TOP)/transforms/python-rename
 PUBLISH_TRANSFORMS +=	$(WS_TOP)/transforms/python-3-groups
 PUBLISH_TRANSFORMS +=	$(WS_TOP)/transforms/python-3-soabi
 PUBLISH_TRANSFORMS +=	$(WS_TOP)/transforms/python-3-no-32bit
