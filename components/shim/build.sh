fb5a982497cb888b5d41720f35baaea1cdd9f0d9
Dave Miner <dave.miner@oracle.com>
2025-04-21 11:42:59 -0700

    37734201 Update shim to recognize Oracle Solaris Secure Boot CA

--- components/shim/build.sh
+++ components/shim/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Script to build shim binaries on Oracle Linux instance in OCI
-ssh opc@$1 sudo dnf -y install podman
+ssh opc@$1 https_proxy=$https_proxy sudo dnf -y install podman
 scp linux_build/* opc@$1:
 ssh opc@$1 https_proxy=$https_proxy podman build -f Dockerfile --output /var/tmp/build
 scp opc@$1:/var/tmp/build/root/rpmbuild/BUILD/shim-15.8/build-x64/*.efi files 
