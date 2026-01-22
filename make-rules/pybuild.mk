6a84f6cd149e9756280cc8d49ac15c261a8bc5ed
Jakub Kulik <jakub.kulik@oracle.com>
2026-01-22 00:43:52 -0800

    38852706 move commonly used Python build backends into REQUIRED_PACKAGES in a shared Makefile

--- make-rules/pybuild.mk
+++ make-rules/pybuild.mk
@@ -20,7 +20,7 @@
 #
 
 #
-# Copyright (c) 2022, 2025, Oracle and/or its affiliates.
+# Copyright (c) 2022, 2026, Oracle and/or its affiliates.
 #
 
 include $(WS_MAKE_RULES)/python-common.mk
@@ -58,9 +58,19 @@ ifneq ($(findstring 3.11, $(PYTHON_VERSIONS)),)
 REQUIRED_PACKAGES += library/python/build-311
 REQUIRED_PACKAGES += library/python/installer-311
 REQUIRED_PACKAGES += library/python/wheel-311
+
+# additional widely used PEP 517 build backends and plugins
+REQUIRED_PACKAGES += library/python/flit_core-311
+REQUIRED_PACKAGES += library/python/hatch-vcs-311
+REQUIRED_PACKAGES += library/python/hatchling-311
 endif
 ifneq ($(findstring 3.13, $(PYTHON_VERSIONS)),)
 REQUIRED_PACKAGES += library/python/build-313
 REQUIRED_PACKAGES += library/python/installer-313
 REQUIRED_PACKAGES += library/python/wheel-313
+
+# additional widely used PEP 517 build backends and plugins
+REQUIRED_PACKAGES += library/python/flit_core-313
+REQUIRED_PACKAGES += library/python/hatch-vcs-313
+REQUIRED_PACKAGES += library/python/hatchling-313
 endif
